afa8dddcf0556e535c512c514d48799fbbe4269e
SvkCreations <95845696+SvkCreations@users.noreply.github.com>
2022-01-09 17:15:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+
+        // $('#reading').css({ "display": "none" });
+        // jQuery(document).ready(function () {
+        //     var offset = 80; /* pixels you have to scroll to the div show up (fade) [you can change] */
+        //     var duration = 1000; /* Duration of the fade (you can change) */
+        //     jQuery(window).scroll(function () {
+        //         if (jQuery(this).scrollTop() > offset) {
+        //             jQuery('#reading').fadeIn(duration);
+        //         } 
+        //     });
+
+        // });
+        // $('#features').css({ "display": "none" });
+        // jQuery(document).ready(function () {
+        //     var offset = 600; /* pixels you have to scroll to the div show up (fade) [you can change] */
+        //     var duration = 1000; /* Duration of the fade (you can change) */
+        //     jQuery(window).scroll(function () {
+        //         if (jQuery(this).scrollTop() > offset) {
+        //             jQuery('#features').fadeIn(duration);
+        //         }
+        //     });
+
+        // });
+        // $('#books').css({ "display": "none" });
+        // jQuery(document).ready(function () {
+        //     var offset = 1200; /* pixels you have to scroll to the div show up (fade) [you can change] */
+        //     var duration = 1000; /* Duration of the fade (you can change) */
+        //     jQuery(window).scroll(function () {
+        //         if (jQuery(this).scrollTop() > offset) {
+        //             jQuery('#books').fadeIn(duration);
+        //         }
+        //     });
+
+        // });
+        // $('#book-1').css({ "display": "none" });
+        // jQuery(document).ready(function () {
+        //     var offset = 1450; /* pixels you have to scroll to the div show up (fade) [you can change] */
+        //     var duration = 1000; /* Duration of the fade (you can change) */
+        //     jQuery(window).scroll(function () {
+        //         if (jQuery(this).scrollTop() > offset) {
+        //             jQuery('#book-1').fadeIn(duration);
+        //         }
+        //     });
+
+        // });
+        // $('#book-2').css({ "display": "none" });
+        // jQuery(document).ready(function () {
+        //     var offset = 2550; /* pixels you have to scroll to the div show up (fade) [you can change] */
+        //     var duration = 1000; /* Duration of the fade (you can change) */
+        //     jQuery(window).scroll(function () {
+        //         if (jQuery(this).scrollTop() > offset) {
+        //             jQuery('#book-2').fadeIn(duration);
+        //         }
+        //     });
+
+        // });
+        // $('#book-3').css({ "display": "none" });
+        // jQuery(document).ready(function () {
+        //     var offset = 3200; /* pixels you have to scroll to the div show up (fade) [you can change] */
+        //     var duration = 1000; /* Duration of the fade (you can change) */
+        //     jQuery(window).scroll(function () {
+        //         if (jQuery(this).scrollTop() > offset) {
+        //             jQuery('#book-3').fadeIn(duration);
+        //         }
+        //     });
+
+        // });
+
+        function mouseOver1(x){
+            x.style.color="#d4747b";
+            x.style.transitionDuration=".5s"
+        }
+        function mouseOut1(x){
+            x.style.color="#9b9d9e";
+            x.style.transitionDuration=".5s"
+        }
